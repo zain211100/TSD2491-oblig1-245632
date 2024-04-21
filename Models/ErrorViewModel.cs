a8a017f77098e5741bd1ae95535103fefe2e1b41
zain211100 <zain-muhammed@outlook.com>
2024-04-21 18:22:34 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TSD2491_oblig1_245632.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
